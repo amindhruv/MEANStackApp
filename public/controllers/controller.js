556538e8f8dfec3d43208036fdc1fa91e65c5c7a
Dhruv Amin <amin.dhruv@gmail.com>
2017-06-25 15:21:33 -0400

    Added files

---
+++ public/controllers/controller.js
@@ -0,0 +1,39 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+	var refresh = function() {
+		$http.get('/contactlist').success(function(response) {
+			$scope.contactlist = response;
+			$scope.contact = "";
+		});
+	};
+
+	refresh();
+
+	$scope.add = function() {
+		$http.post('/contactlist', $scope.contact).success(function(response) {
+			refresh();
+		});
+	};
+
+	$scope.update = function() {
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+			refresh();
+		});
+	};
+
+	$scope.clear = function() {
+		$scope.contact = "";
+	};
+
+	$scope.remove = function(id) {
+		$http.delete('/contactlist/' + id).success(function(response) {
+			refresh();
+		});
+	};
+
+	$scope.edit = function(id) {
+		$http.get('/contactlist/' + id).success(function(response) {
+			$scope.contact = response;
+		});
+	};
+}]);
